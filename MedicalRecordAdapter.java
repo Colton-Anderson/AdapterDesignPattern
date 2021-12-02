107dfa66d47187a7158589d2d7a7b6280cf60df3
Colton-Anderson <coltonanderson10@gmail.com>
2021-12-02 15:45:44 -0500

    initial code commit

---
+++ MedicalRecordAdapter.java
@@ -0,0 +1,35 @@
+public class MedicalRecordAdapter {
+    private HealthRecord record;
+
+    public MedicalRecordAdapter(HealthRecord record) {
+
+    }
+
+    public String getFirstName() {
+
+    }
+
+    public String getLastName() {
+
+    }
+
+    public Date getBirthdate() {
+
+    }
+
+    public Gender getGender() {
+
+    }
+
+    public void addVisit(Date date, boolean well, String description) {
+
+    }
+
+    public ArrayList<Visit> getVisitHistory() {
+
+    }
+
+    public String toString() {
+        
+    }
+}
